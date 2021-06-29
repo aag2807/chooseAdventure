9401dc32ba4fd551d814e4161eac16b92e0ce96f
Alvaro Guzman <alvaro.guzman@ctc.edu.do>
2021-06-29 13:35:30 -0400

    added some styles to template

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	"makeAdventure"
 )
 
 func main() {
+	port := flag.Int("port", 7070, "the default port")
 	fileName := flag.String("File", "gopher.json", "The json file with the story")
 	flag.Parse()
 	f, err := os.Open(*fileName)
@@ -21,6 +24,7 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println(story)
-
+	handler := makeAdventure.NewHandler(story)
+	fmt.Printf("running in port %d \n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), handler))
 }
--- story.go
+++ story.go
@@ -2,8 +2,73 @@ package makeAdventure
 
 import (
 	"encoding/json"
+	"html/template"
 	"io"
+	"log"
+	"net/http"
+	"strings"
 )
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.New("").Parse(defaultHandlerTemplate))
+}
+
+var defaultHandlerTemplate = `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Make adventure</title>
+</head>
+
+<body>
+	<div style="width:80%; max-width:800px; margin:auto; margin-top:40px; margin-bottom: 40px; background:#fffcf6; box-shadow: 0 10px 6px -6px #777">
+		<h1>{{.Title}}</h1>
+		{{range .Paragraphs}}
+		<p style="color:blue;">{{.}}</p>
+		{{end}}
+		<ul style="border-top: 1px dotted #ccc; padding: 10px 0 0 0; --webkit-padding-start:0;list-style-type:none;">
+			{{range .Options}}
+			<li style="padding-top:10px"><a href="/{{.Chapter}}"> {{.Text}}</a> </li>
+			{{end}}
+		</ul>
+	</div>
+</body>
+
+</html>
+`
+
+func NewHandler(s Story) Handler {
+	return Handler{s}
+}
+
+type Handler struct {
+	s Story
+}
+
+func (h Handler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	path := strings.TrimSpace(req.URL.Path)
+
+	if path == "" || path == "/" {
+		path = "/intro"
+	}
+
+	path = path[1:]
+	if chapter, ok := h.s[path]; ok {
+		err := tpl.Execute(res, chapter)
+		if err != nil {
+			log.Println(err)
+			http.Error(res, "something went wrong...", http.StatusInternalServerError)
+		}
+		return
+	}
+	http.Error(res, "Chapter not found.", http.StatusNotFound)
+}
 
 type Story map[string]Chapter
 
